4ad3bc714deea201743c70ae915232b748f6ba52
Akshaya HP <akshayhulkuli@gmail.com>
2024-05-29 20:31:38 +0530

    saving changes

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./todoApp.css";
 
 const TodoApp = () => {
   return (
     <div>
-      <LoginComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />}></Route>
+          <Route path="/login" element={<LoginComponent />}></Route>
+          <Route path="/home" element={<WelcomeComponent />}></Route>
+          <Route path="*" element={<ErrorComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
 
 const LoginComponent = () => {
+  const navigate = useNavigate();
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
@@ -21,6 +30,7 @@ const LoginComponent = () => {
     if (userName === "Akshay" && password === "qwerty") {
       setShowSuccessMessage(true);
       setShowFailureMessage(false);
+      navigate("/home");
     } else {
       setShowSuccessMessage(false);
       setShowFailureMessage(true);
@@ -29,8 +39,16 @@ const LoginComponent = () => {
 
   return (
     <div>
-      <LoginSuccessMessage showSuccessMessage={showSuccessMessage} />
-      <LoginFailureMessage showFailureMessage={showFailureMessage} />
+        <h1>Time to login</h1>
+      {showSuccessMessage && (
+        <div className="login-success">Authenticaed successfully</div>
+      )}
+      {showFailureMessage && (
+        <div className="login-failure">
+          {" "}
+          Authentication failed, please check crendentails
+        </div>
+      )}
       <form className="login-form" onSubmit={handleSubmit}>
         <div className="input-field">
           <label style={{ margin: "10px", padding: "10px" }}>UserName :</label>{" "}
@@ -64,24 +82,17 @@ const LoginComponent = () => {
   );
 };
 
-const LoginSuccessMessage = (props) => {
-  if (props.showSuccessMessage)
-    return <div className="login-success">Authenticaed successfully</div>;
-  return <></>;
-};
-
-const LoginFailureMessage = (props) => {
-  if (props.showFailureMessage)
-    return (
-      <div className="login-failure">
-        Authentication failed, please check crendentails
-      </div>
-    );
-  return <></>;
-};
-
 const WelcomeComponent = () => {
   return <div>Welcome to the Todo app</div>;
+};
+
+const ErrorComponent = () => {
+  return (
+    <div>
+      <h1>Ooops !!</h1>
+      <p>Something went wrong, contact admin</p>
+    </div>
+  );
 };
 
 export default TodoApp;
